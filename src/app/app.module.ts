84c1c9cf3128e4e9847b01012ede73da6ee40228
AdamDono <111118088+AdamDono@users.noreply.github.com>
2023-05-23 15:51:52 +0200

    installed node models

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-
+import { ViewChild } from '@angular/core'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -35,7 +34,7 @@ import {MatSortModule} from '@angular/material/sort';
      MatPaginatorModule,
     HttpClientModule,
     MatSortModule,
-    MatInputModule
+    MatInputModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/coin-details/coin-details.component.ts
+++ src/app/coin-details/coin-details.component.ts
@@ -1,7 +1,10 @@
 import { ApiService } from './../servi/api.service';
 import { HttpClient } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import {ChartConfiguration, ChartType} from 'chart.js';
+import{BaseChartDirective} from 'ng2-charts'
+import { NgChartsModule } from 'ng2-charts';
 
 @Component({
   selector: 'app-coin-details',
@@ -14,6 +17,38 @@ export class CoinDetailsComponent implements OnInit {
   coinID ! :string;
   days : number= 1; 
   currency : string = 'USD';
+
+
+  public lineChartData: ChartConfiguration['data'] = {
+    datasets: [
+      {
+        data: [],
+        label: `Price Trends`,
+        backgroundColor: 'rgba(148,159,177,0.2)',
+        borderColor: '#009688',
+        pointBackgroundColor: '#009688',
+        pointBorderColor: '#009688',
+        pointHoverBackgroundColor: '#009688',
+        pointHoverBorderColor: '#009688',
+
+      }
+    ],
+    labels: []
+  };
+  public lineChartOptions: ChartConfiguration['options'] = {
+    elements: {
+      point: {
+        radius: 1
+      }
+    },
+
+    plugins: {
+      legend: { display: true },
+    }
+  };
+  public lineChartType: ChartType = 'line';
+  @ViewChild(BaseChartDirective) myLineChart !: BaseChartDirective;
+
 
   constructor(private api : ApiService, private activatedRoute : ActivatedRoute  ) { }
 
